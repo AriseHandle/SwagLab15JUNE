5a543b689b82b3db8ce078603bd1f1fc31a5612b
SRShahir <shreyashahir20@gmail.com>
2023-11-02 23:22:47 +0530

    Change in parameterization

--- src/main/java/utility/Parametrization.java
+++ src/main/java/utility/Parametrization.java
@@ -7,7 +7,7 @@ import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Parametrization {
-	public static String main(String[] args, int row, String SheetName, int cell) throws EncryptedDocumentException, IOException {
+	public static String parametrization(int row, String SheetName, int cell) throws EncryptedDocumentException, IOException {
 		FileInputStream file=new FileInputStream("D:\\selenioum\\Practice Excell Sheet.xlsx");
 		String value=WorkbookFactory.create(file).getSheet(SheetName).getRow(row).getCell(cell).getStringCellValue();
 		return value;
