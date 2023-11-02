982adac60638aa1c5f2453d3d317b48595992907
SRShahir <shreyashahir20@gmail.com>
2023-11-02 22:36:10 +0530

    Create Parametrization and ScreenShot

---
+++ src/main/java/utility/Parametrization.java
@@ -0,0 +1,17 @@
+package utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Parametrization {
+	public static String main(String[] args, int row, String SheetName, int cell) throws EncryptedDocumentException, IOException {
+		FileInputStream file=new FileInputStream("D:\\selenioum\\Practice Excell Sheet.xlsx");
+		String value=WorkbookFactory.create(file).getSheet(SheetName).getRow(row).getCell(cell).getStringCellValue();
+		return value;
+		
+	}
+
+}
